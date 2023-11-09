c04ebf69bfcd855a91754648f663cbb3c4098624
gtorresv <giselle.torres15@gmail.com>
2023-11-08 21:45:25 -0500

    deleted a script

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,9 +4,6 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    // user: async (parent, { _id, username }) => {
-    //   return User.findOne({ username });
-    // },
     me: async (parent, args, context) => {
         if (context.user) {
           return User.findOne({ _id: context.user._id });
